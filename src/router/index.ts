3e6aa6a81d9e1925c175235117ebd8f0ded47df1
BunnyBit <f411024@gmail.com>
2021-12-08 00:01:33 +0800

    feat: 加入 Episode 和 Home 兩空白頁 並 設定 Vue-Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [
+type Routes = Parameters<typeof createRouter>['0']['routes'];
+
+const routes: Routes = [
   {
     path: '/',
-    component: () => import('/@/pages/Index.vue'),
+    component: () => import('../pages/Home/Index.vue'),
+  },
+  {
+    path: '/episode',
+    component: () => import('/@/pages/Episode/Episode.vue'),
   },
 ];
 
 export const Router = createRouter({
-  scrollBehavior: () => ({ left: 0, top: 0 }),
   history: createWebHistory(),
   routes,
 });
