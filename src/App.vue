a87558a4e5ce129bacce892f229ef72b2c86add0
BunnyBit <f411024@gmail.com>
2021-12-08 02:11:48 +0800

    feat: 加入 head navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'App',
-});
-</script>
 <template>
+  <Header></Header>
   <router-view />
 </template>
+
+<script lang="ts" setup></script>
