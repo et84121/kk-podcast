5cf6fa5dd2845dbd6a046e8dea7e3a569974d9b4
BunnyBit-beehive <f411024@gmail.com>
2021-12-08 16:39:25 +0800

    refactor(vue-router): 為 mock vue-router export routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 type Routes = Parameters<typeof createRouter>['0']['routes'];
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     name: 'home',
     path: '/',
