608e27bf579f60da9602de806f96332904797c0f
BunnyBit <f411024@gmail.com>
2021-12-13 02:59:56 +0800

    test(Episode): WIP

--- src/__TEST__/unit/Episode.spec.ts
+++ src/__TEST__/unit/Episode.spec.ts
@@ -1,23 +1,76 @@
-import { describe, expect, beforeEach, it, test } from '@jest/globals';
+import { describe, expect, beforeEach, it, test, jest } from '@jest/globals';
 import { mount } from '@vue/test-utils';
+import { createRouterMock, injectRouterMock } from 'vue-router-mock';
+import { routes } from '/@/router';
+import { createTestingPinia } from '@pinia/testing';
 import EpisodeVue from '/@/pages/Episode/Episode.vue';
+import { usePodcastChannelStore } from '/@/store/podcastChannelStore';
+import { episodeFactory, podcastChannelFactory } from './mockDataFactory';
+import { podcastPlayerPlugin, usePodcastPlayer } from '/@/plugin/PodcastPlayer';
+import { RouterLink } from 'vue-router';
 
-describe('Header.vue', () => {
-  it('有一個 episode-image 元素', async () => {
-    const wrapper = mount(EpisodeVue);
+function factory() {
+  jest.mock('/@/plugin/PodcastPlayer');
 
-    expect(wrapper.find("[data-test='episode-image']")).toBeTruthy();
+  const motionSlideBottom = jest.fn();
+
+  const episodes = episodeFactory.batchSync(3);
+
+  const episode = episodes[0];
+
+  const wrapper = mount(EpisodeVue, {
+    props: { guid: episodes[0].guid },
+    global: {
+      plugins: [createTestingPinia(), podcastPlayerPlugin],
+      components: { 'router-link': RouterLink },
+      directives: {
+        'motion-slide-bottom': motionSlideBottom,
+      },
+    },
   });
 
-  it('有一個 episode-meta 元素', async () => {
-    const wrapper = mount(EpisodeVue);
+  const store = usePodcastChannelStore();
 
-    expect(wrapper.find("[data-test='episode-meta']")).toBeTruthy();
+  store.channel = podcastChannelFactory.buildSync({
+    items: episodes,
   });
 
-  it('有一個 episode-play 元素', async () => {
-    const wrapper = mount(EpisodeVue);
+  const podcastPlayer = usePodcastPlayer();
 
-    expect(wrapper.find("[data-test='episode-play']")).toBeTruthy();
+  return {
+    wrapper,
+    store,
+    episodes,
+    episode,
+    podcastPlayer,
+    motionSlideBottom,
+  };
+}
+
+describe('Episode.vue', () => {
+  const router = createRouterMock({
+    // mock vue-router with real routes
+    routes: routes,
+  });
+  beforeEach(() => {
+    injectRouterMock(router);
+  });
+
+  it('image', async () => {
+    const { wrapper, episode } = factory();
+
+    expect(
+      wrapper.find("[data-test='episode-image']").find('img').attributes('src'),
+    ).toBe(episode.itunes.image);
+  });
+
+  it('episode-play', async () => {
+    const { wrapper, episode, podcastPlayer } = factory();
+
+    const playButton = wrapper.find("[data-test='episode-play']");
+
+    await playButton.trigger('click');
+
+    expect(podcastPlayer.play).toBeCalledWith(episode.guid);
   });
 });
