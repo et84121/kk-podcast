1aa542a631dd1baf20c568d82ab4c1db330193b1
BunnyBit <f411024@gmail.com>
2021-12-12 10:00:02 +0800

    feat: BottomPlayer 完整功能

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <Header></Header>
-  <router-view />
+  <div :class="{ 'pb-20': playerVisbility }">
+    <router-view />
+  </div>
 
-  <BottomPlayer></BottomPlayer>
+  <BottomPlayer v-model:visbility="playerVisbility"></BottomPlayer>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { usePodcastChannelStore } from './store/podcastChannelStore';
 const podcastChannelStore = usePodcastChannelStore();
 // load rss feed
 podcastChannelStore.load();
 
+const playerVisbility = ref(false);
 </script>
--- src/pages/common/BottomPlayer.vue
+++ src/pages/common/BottomPlayer.vue
@@ -9,30 +9,111 @@
       items-center
       gap-2
       px-16
-      py-4
+      pt-6
+      pb-4
+      bg-white
     "
+    :class="{
+      hidden: !visbility,
+    }"
   >
     <!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range -->
-    <input
-      class="absolute -top-2 right-0 left-0 w-full"
-      type="range"
-      min="0"
-      max="100"
-      step="any"
-      data-test="soundplayer-seek-bar"
-    />
+    <div class="absolute -top-2 right-0 left-0 w-full flex flex-row">
+      <p class="mx-2 whitespace-nowrap">
+        {{
+          formatDuration(
+            { minutes: 0, seconds: Math.round(currentTime) },
+            { locale: zhTW },
+          )
+        }}
+      </p>
+      <input
+        v-model="currentTime"
+        class="w-full"
+        type="range"
+        min="0"
+        :max="duration"
+        step="any"
+        data-test="soundplayer-seek-bar"
+      />
+      <p class="mx-2">{{ Math.round(duration) }}</p>
+    </div>
 
     <button
-      class="rounded-full border border-black border-4 w-10 h-10"
+      v-if="status.playing"
+      class="playerButton"
       data-test="soundplayer-state-controller"
+      :disabled="!playable"
+      @click="pause()"
     >
-      II
+      Pause
     </button>
 
-    <p class="text-lg" data-test="soundplayer-state-info">
-      Now Playing... Ep. Name
-    </p>
+    <button
+      v-else
+      class="playerButton"
+      data-test="soundplayer-state-controller"
+      :disabled="!playable"
+      @click="play()"
+    >
+      Play
+    </button>
+
+    <div
+      v-if="episode"
+      data-test="soundplayer-state-info"
+      class="text-lg flex flex-row flex-wrap gap-4"
+    >
+      <p>Now Playing...</p>
+      <p>
+        <router-link :to="{ name: 'episode', params: { guid: episode.guid } }">
+          {{ episode.title }}
+        </router-link>
+      </p>
+    </div>
+
+    <p v-else>請選擇集數</p>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { usePodcastPlayer } from '/@/plugin/PodcastPlayer';
+import { computed, ref, watch } from 'vue';
+import { formatDuration } from 'date-fns';
+import { zhTW } from 'date-fns/locale';
+
+const props = withDefaults(defineProps<{ visbility?: boolean }>(), {
+  visbility: false,
+});
+
+const emit = defineEmits<{ (e: 'update:visbility', value: boolean): void }>();
+
+const visbility = computed({
+  get: () => props.visbility,
+  set: val => emit('update:visbility', val),
+});
+
+const podcastPlayer = usePodcastPlayer();
+
+const { currentTime, duration, play, pause, status } = podcastPlayer;
+
+const episode = podcastPlayer.EpisodeMeta;
+
+const playable = computed(() => {
+  return Boolean(episode.value);
+});
+
+watch(
+  playable,
+  newVal => {
+    if (newVal) visbility.value = true;
+  },
+  { immediate: true },
+);
+</script>
+
+<style>
+.playerButton {
+  @apply rounded-full border border-black border-2 w-20 h-10  disabled:(text-gray-400 border-gray-500 ) hover:();
+}
+</style>
