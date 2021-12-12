c8d8af55f0b3a9de7f082e007eb6b2491170bd67
BunnyBit <f411024@gmail.com>
2021-12-12 09:40:00 +0800

    feat: Vue app install  podcastPlayer plugin

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,15 @@ import 'virtual:windi-devtools';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
+//podcastPlayer
+import { podcastPlayer } from '/@/plugin/PodcastPlayer';
+
 const app = createApp(App);
 
 app.use(createPinia().use(piniaPluginPersistedstate));
 
 app.use(Router);
+
+app.use(podcastPlayer);
 
 app.mount('#app');
